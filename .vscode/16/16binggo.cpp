1cc3e0af403dc4c6775f6ab3a5aca3ff61c9d502
HJSHIN92 <tls0901kr@gmail.com>
2021-06-30 16:48:52 +0900

    Add selected

--- .vscode/16/16binggo.cpp
+++ .vscode/16/16binggo.cpp
@@ -24,11 +24,11 @@ int main(){
         iNumber[idx2] = iTemp;
     }
 
-
+   int vertical, horizon, diagonal, iTotal;
     // 게임시작
     while (true)
     {
-        cout << "1 ~ 24까지의 숫자를 입력해주세요. (종료 : 0)" << endl;
+        cout << "1 ~ 25까지의 숫자를 입력해주세요. (종료 : 0)" << endl;
         int iInput; // 플레이어의 입력값
         cin >> iInput;
     // 게임이 진행되지않는 예외사항을 먼저 적는다.
@@ -38,13 +38,23 @@ int main(){
         cout << " 게임을 종료합니다 " << endl;
         break;
     }
-    // 2. 1~24 사이의 숫자가 아닌 경우
+    // 2. 1~25 사이의 숫자가 아닌 경우
     else if(!(iInput > 0 && iInput < 26))
     {
-        cout << "1부터 24까지의 숫자로 다시 입력하세요" << endl;
+        cout << "1부터 25까지의 숫자로 다시 입력하세요" << endl;
         continue;
     }
 
+    int iSelected[25] = {};
+    for(int i = 0; i < 25; ++i)
+    {
+        iSelected[i] = i + 1;
+        if(iSelected[i] == iInput)
+        {
+            cout << " 중복된 숫자입니다. 다시 선택해주세요" << endl;
+            continue;
+        }
+    }
     // 빙고판 보여주기
     for(int i = 0; i < 5; ++i)
     {
@@ -54,6 +64,7 @@ int main(){
             {
                 cout << " * " << "\t";
                 iNumber[i*5 + j] = INT_MAX;
+                iNumber[i*5 + j] = iSelected[i*5 + j];
             }
             else if(iNumber[i*5 + j] != INT_MAX)
             {
@@ -67,34 +78,58 @@ int main(){
     // boolean을 사용해서 true로 선언한 뒤
     // 해당 조건을 만족해야 true, 아니면 false
     // if( bool == true ) { ++ ver, ho, dia }
-    int vertical, horizon, diagonal;
-    bool bver, bhor, bdia = true;
-    
+ 
+ 
+    vertical = horizon = diagonal= 0;
+
     // 1) 세로빙고
     for(int i = 0; i < 5; ++i)     // 0, 1, 2, 3, 4
-    {   for(int j = 0; j < 5; ++j) // 0, 1, 2, 3, 4
+    {   
+        if(iNumber[i] == iNumber[i+5] == iNumber[i+10]== iNumber[i+15]== iNumber[i+20])
         {
-            if(iNumber[i] != iNumber[i + 5*j])
-            {
-                bver = false;
-                break;
-            }
+            ++vertical;
         }
-/* 1) 세로빙고
-if(iNumber[i] == inumber[5*(j+1)])
+/*
+0  1  2  3  4
+5  6  7  8  9
+10 11 12 13 14
+15 16 17 18 19
+20 21 22 23 24
+*/
+// 2) 가로빙고
+    if( (i%5==0)
+    && (iNumber[i]==iNumber[i+1]==iNumber[i+2]==iNumber[i+3]==iNumber[i+4] ))
+        {
+            ++horizon;
+        }
+//대각선 빙고
+    // if(iNumber[0] == iNumber[6*i] == iNumber[6*2*i] == iNumber[6*3*i] == iNumber[6*4*i])
+    //     {
+    //         ++diagonal;
+    //     }
+    // if(iNumber[4] == iNumber[4*2*i] == iNumber[4*3*i] == iNumber[4*4*i] == iNumber[4*5*i])
+    //     {
+    //         ++diagonal;
+    //     }
+    }
+    if(iNumber[0] == iNumber[6] == iNumber[12] == iNumber[18] == iNumber[24])
+        {
+            ++diagonal;
+        }
+    if(iNumber[4] == iNumber[8] == iNumber[12] == iNumber[16] == iNumber[20])
+        {
+            ++diagonal;
+        }
+
+    // 빙고 결과 체크
+    iTotal = vertical + horizon + diagonal;
+    cout << "가로 :" << horizon << "\t" << "세로 :" << vertical << "\t" << "대각선 :" << diagonal 
+    << "\t" << "총 :" << iTotal << endl;
+
+    if(iTotal >= 5)
     {
-    ++vertical;
+        cout << "축하합니다, 게임을 종료합니다" << endl;
+        break;
     }
-2) 가로빙고
-if( (i%5==0 && (i+5)%5 ==0)
-   && (iNumber[i]==iNumber[j+1]) )
-
-3) 대각선 빙고
-if( (iNumber[i+ 6*j])
-    && (iNumber[4*(j+1)])))
-    ++ diagonal
-*/       
-    }
-
 }
 }
